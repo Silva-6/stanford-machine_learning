dad3abcbb3422f64746d76b4c08a6a03136d93b6
Silva-6 <silva6media@gmail.com>
2024-05-07 16:17:11 +0100

    softmax placement and ReLU activation

---
+++ 01-advanced_learning_algorithms/practice_lab_02-neural_networks_for_multiclass_classification/00-numpy_implement_softmax.py
@@ -0,0 +1,30 @@
+from pkgs import *
+# UNQ_C1
+
+
+def my_softmax(z):
+    """ Softmax converts a vector of values to a probability distribution.
+    Args:
+      z (ndarray (N,))  : input data, N features
+    Returns:
+      a (ndarray (N,))  : softmax of z
+    """
+
+    def my_softmax(z):
+        N = len(z)
+
+        a = np.zeros(N)
+        ez_sum = 0
+        for k in range(N):
+            ez_sum += np.exp(z[k])
+        for j in range(N):
+            a[j] = np.exp(z[j]) / ez_sum
+
+        return a
+
+# Or, a vector implementation:
+
+# def my_softmax(z):
+#    ez = np.exp(z)
+#    a = ez / np.sum(ez)
+#    return (a)
---
+++ 01-advanced_learning_algorithms/practice_lab_02-neural_networks_for_multiclass_classification/01-ReLU_three_layer_network.py
@@ -0,0 +1,11 @@
+from pkgs import *
+# UNQ_C2
+
+tf.random.set_seed(1234)  # for consistent results
+model = Sequential([
+    tf.keras.Input(shape=(400,)),     # @REPLACE
+    Dense(25, activation='relu', name = "L1"),  # @REPLACE
+    Dense(15, activation='relu',  name = "L2"),  # @REPLACE
+    Dense(10, activation='linear', name = "L3"),  # @REPLACE
+    ], name="my_model"
+)
