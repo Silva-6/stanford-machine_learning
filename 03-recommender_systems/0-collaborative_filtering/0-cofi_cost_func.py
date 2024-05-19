07a6773bcefed1216aff39f773f6d2ab6834d2a6
Silva-6 <silva6media@gmail.com>
2024-05-19 06:18:31 +0100

    collaborative filtering

---
+++ 03-recommender_systems/0-collaborative_filtering/0-cofi_cost_func.py
@@ -0,0 +1,53 @@
+# GRADED FUNCTION: cofi_cost_func
+# UNQ_C1
+
+def cofi_cost_func(X, W, b, Y, R, lambda_):
+    """
+    Returns the cost for the collaborative filtering
+    Args:
+      X (ndarray (num_movies,num_features)): matrix of item features
+      W (ndarray (num_users,num_features)) : matrix of user parameters
+      b (ndarray (1, num_users)            : vector of user parameters
+      Y (ndarray (num_movies,num_users)    : matrix of user ratings of movies
+      R (ndarray (num_movies,num_users)    : matrix, where R(i, j) = 1 if the i-th movies was rated by the j-th user
+      lambda_ (float): regularization parameter
+    Returns:
+      J (float) : Cost
+    """
+    nm, nu = Y.shape
+    J = 0
+
+    for j in range(nu):
+        w = W[j, :]
+        b_j = b[0, j]
+        for i in range(nm):
+            x = X[i, :]
+            y = Y[i, j]
+            r = R[i, j]
+            J += np.square(r * (np.dot(w, x) + b_j - y))
+    J = J / 2
+
+    return J
+
+# Reduce the data set size so that this runs faster
+num_users_r = 4
+num_movies_r = 5
+num_features_r = 3
+
+X_r = X[:num_movies_r, :num_features_r]
+W_r = W[:num_users_r,  :num_features_r]
+b_r = b[0, :num_users_r].reshape(1,-1)
+Y_r = Y[:num_movies_r, :num_users_r]
+R_r = R[:num_movies_r, :num_users_r]
+
+# Evaluate cost function
+J = cofi_cost_func(X_r, W_r, b_r, Y_r, R_r, 0);
+print(f"Cost: {J:0.2f}")
+
+# Evaluate cost function with regularization
+J = cofi_cost_func(X_r, W_r, b_r, Y_r, R_r, 1.5);
+print(f"Cost (with regularization): {J:0.2f}")
+
+# Public tests
+from public_tests import *
+test_cofi_cost_func(cofi_cost_func)
