bb2a39815777dd8acd3cb97714788ce8dcd93e44
Silva-6 <silva6media@gmail.com>
2024-05-21 01:12:35 +0100

    compute the square distance

---
+++ 03-recommender_systems/1-content_based_filtering/2-compute_sq_dist.py
@@ -0,0 +1,21 @@
+# GRADED_FUNCTION: sq_dist
+# UNQ_C2
+def sq_dist(a,b):
+    """
+    Returns the squared distance between two vectors
+    Args:
+      a (ndarray (n,)): vector with n features
+      b (ndarray (n,)): vector with n features
+    Returns:
+      d (float) : distance
+    """
+    d_sq = [(i-j)**2 for i,j in zip(a,b)]
+    d = sum(d_sq)
+    return d
+
+a1 = np.array([1.0, 2.0, 3.0]); b1 = np.array([1.0, 2.0, 3.0])
+a2 = np.array([1.1, 2.1, 3.1]); b2 = np.array([1.0, 2.0, 3.0])
+a3 = np.array([0, 1, 0]);       b3 = np.array([1, 0, 0])
+print(f"squared distance between a1 and b1: {sq_dist(a1, b1):0.3f}")
+print(f"squared distance between a2 and b2: {sq_dist(a2, b2):0.3f}")
+print(f"squared distance between a3 and b3: {sq_dist(a3, b3):0.3f}")
