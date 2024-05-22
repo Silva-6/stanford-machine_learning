01447396dfd7e1fcef20ce234d199271e9fd9d74
Silva-6 <silva6media@gmail.com>
2024-05-22 18:31:12 +0100

    update the weights of the  𝑄   and target  𝑄̂   networks using a custom training loop

---
+++ 04-reinforcement_learning/1-practice_lab-virtual_lunar_lander/2-agent_learn.py
@@ -0,0 +1,23 @@
+@tf.function
+def agent_learn(experiences, gamma):
+    """
+    Updates the weights of the Q networks.
+
+    Args:
+      experiences: (tuple) tuple of ["state", "action", "reward", "next_state", "done"] namedtuples
+      gamma: (float) The discount factor.
+
+    """
+
+    # Calculate the loss
+    with tf.GradientTape() as tape:
+        loss = compute_loss(experiences, gamma, q_network, target_q_network)
+
+    # Get the gradients of the loss with respect to the weights.
+    gradients = tape.gradient(loss, q_network.trainable_variables)
+
+    # Update the weights of the q_network.
+    optimizer.apply_gradients(zip(gradients, q_network.trainable_variables))
+
+    # update the weights of target q_network
+    utils.update_target_network(q_network, target_q_network)
